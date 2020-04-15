7f96f8bacbe01ad166987e3b8f001c69be18d63d
Le Minh Nhat Dang <61152480+lmndang@users.noreply.github.com>
2020-04-15 14:38:56 -0400

    Create new modal

---
+++ menu_helper.h
@@ -0,0 +1,31 @@
+/* DIGITAL SIGNATURE(S):
+   ====================
+
+   List the student author(s) of this code below:
+
+   Fullname                    Seneca Email Address
+   --------------------------- ----------------------------
+   1) Le Minh Nhat Dang        lmndang@myseneca.ca
+   2)
+   3)
+
+   +--------------------------------------------------------+
+   |                   FILE: main.c                         |
+   +--------------------------------------------------------+
+   |           2  0  2  0  ~  W  I  N  T  E  R              |
+   |                 I  P  C  :  B  T  P                    |
+   |                 1  4  4  :  1  0  0                    |
+   |              FINAL ASSESSMENT PART - 2                 |
+   |                                                        |
+   |     S  E  N  E  C  A            C  O  L  L  E  G  E    |
+   +--------------------------------------------------------+ */
+
+void clearKeyboard(void);
+
+int getInt(void);
+
+int getIntInRange(int min, int max);
+
+int menu(void);
+
+void managerSystem(void);
